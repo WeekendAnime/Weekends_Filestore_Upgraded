451a9916c994520716bf772224638babb02174d2
Jeffrey Moses <kalavathy721945@gmail.com>
2024-11-23 15:40:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -107,8 +107,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data="about"),
-                    InlineKeyboardButton("🍁 sᴇʀɪᴇsғʟɪx", url="https://t.me/Team_Netflix/40"),
+                    InlineKeyboardButton("⚡️ Λвσυт", callback_data="about"),
+                    InlineKeyboardButton("🍁 Λиιмє ωєєкєи∂ѕ", url="https://t.me/Anime_Weekends"),
                 ]
             ]
         )
